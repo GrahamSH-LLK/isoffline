1668a78b0bd77f0ece735f9eef715061c51517ab
GrahamSH <grahamshllk@gmail.com>
2023-01-16 15:48:52 +0000

    Add prod stuff

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import {
 import { defineCorsEventHandler } from "@nozomuikuta/h3-cors";
 import { listen } from "listhen";
 const client = createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
 });
 
 client.on("error", (err) => {
